20c568e80ba1d593adbc0576607b44c9a73f377a
Jack Cullen <jpcullen20@gmail.com>
2020-06-18 14:12:37 -0600

    Add test for asteroid data list helper

--- near_earth_objects_test.rb
+++ near_earth_objects_test.rb
@@ -8,4 +8,10 @@ class NearEarthObjectsTest < Minitest::Test
     results = NearEarthObjects.find_neos_by_date('2019-03-30')
     assert_equal '(2019 GD4)', results[:astroid_list][0][:name]
   end
+
+  def test_asteriods_list_data_helper
+    results = NearEarthObjects.asteroids_list_data('2019-03-30')
+    assert_equal Faraday::Response, results.class
+    assert_equal String, results.body.class
+  end
 end
