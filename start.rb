023cffc3f57617f3d3f8bbdec761b74e60c394ec
Jack Cullen <jpcullen20@gmail.com>
2020-06-19 10:58:57 -0600

    Refactor start to have a table object

--- start.rb
+++ start.rb
@@ -1,4 +1,5 @@
 require_relative 'near_earth_objects'
+require_relative 'table'
 
 puts "________________________________________________________________________________________________________________________________"
 puts "Welcome to NEO. Here you will find information about how many meteors, astroids, comets pass by the earth every day. \nEnter a date below to get a list of the objects that have passed by the earth on that day."
@@ -7,17 +8,16 @@ print ">>"
 
 date = gets.chomp
 astroid_details = NearEarthObjects.find_neos_by_date(date)
-astroid_list = astroid_details.details[:astroid_list]
 total_number_of_astroids = astroid_details.details[:total_number_of_astroids]
 largest_astroid = astroid_details.details[:biggest_astroid]
-
+table = Table.new(astroid_details)
 
 formated_date = DateTime.parse(date).strftime("%A %b %d, %Y")
 puts "______________________________________________________________________________"
 puts "On #{formated_date}, there were #{total_number_of_astroids} objects that almost collided with the earth."
 puts "The largest of these was #{largest_astroid} ft. in diameter."
 puts "\nHere is a list of objects with details:"
-puts divider
-puts header
-create_rows(astroid_list, column_data)
-puts divider
+puts table.divider
+puts table.header
+table.create_rows
+puts table.divider
